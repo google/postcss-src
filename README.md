10e0c728a4e7f4267de8d3c8ccf3da39c921925e
Natalie Weizenbaum <nweiz@google.com>
2022-08-18 18:27:18 -0700

    Implement `postcss-src`

--- README.md
+++ README.md
@@ -1,29 +1,99 @@
-# Open source PostCSS polyfill for the new src() function:
+# Open source PostCSS polyfill for the new `src()` function
 
-This repo is for PostCSS polyfill for new src() function. 
-https://drafts.csswg.org/css-values/#funcdef-src
+CSS Values and Units Level 4 defines [a `src()` function]. This function works
+just like the old-school `url()` function, but without `url()`'s legacy parsing
+behavior that prevents the use of additional CSS functions like `var()` inside
+it.
 
+[a `src()` function]: https://www.w3.org/TR/css-values-4/#funcdef-src
 
+This plugin polyfills the `src()` function by compiling it to an equivalent
+`url()`, getting rid of the quotes around the string if possible. For example:
 
-## Source Code Headers
+```css
+body {
+  background: src("http://www.example.com/pinkish.gif");
+}
+```
 
-Every file containing source code must include copyright and license
-information. This includes any JS/CSS files that you might be serving out to
-browsers. (This is to help well-intentioned people avoid accidental copying that
-doesn't comply with the license.)
+is transformed into:
 
-Apache header:
+```css
+body {
+  background: url(http://www.example.com/pinkish.gif);
+}
+```
 
-    Copyright 2022 Google LLC
+Although this plugin doesn't natively polyfill CSS variables, it can be used
+with other polyfills like [`postcss-custom-properties`] to make it possible to
+include build-time variable values in `url()`s. But make sure you run
+`postcss-src` last in your plugin chain!
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+[`postcss-custom-properties`]: https://github.com/csstools/postcss-plugins/tree/main/plugins/postcss-custom-properties
 
-        https://www.apache.org/licenses/LICENSE-2.0
+For example:
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+```css
+/* With both postcss-custom-properties and postcss-src: */
+
+:root {
+  --pinkish: "http://www.example.com/pinkish.gif";
+}
+
+body {
+  background: src(var(--pinkish));
+}
+```
+
+is transformed into:
+
+```css
+body {
+  background: url(http://www.example.com/pinkish.gif);
+}
+```
+
+## Usage
+
+Add `postcss-src` to your project:
+
+```sh
+npm install postcss-custom-properties --save-dev
+```
+
+Use `postcss-src` as a PostCSS plugin:
+
+```js
+const postcss = require('postcss');
+const postcssSrc = require('postcss-src);
+
+postcss([
+  postcssSrc(/* plugin options */)
+]).process(css /*, PostCSS options */);
+```
+
+### Options
+
+#### `allowNativeSrc`
+
+By default, if `postcss-src` encounters a `src()` that it can't safely convert
+to a `url()` (such as `src(var(--foo))`), it will throw an error.
+
+If this is `true`, it will emit the `src()` unchanged instead.
+
+```js
+postcssSrc({
+  allowNativeSrc: true,
+});
+```
+
+For example, this throws an error by default but with `allowNativeSrc: true` it
+will be left unchanged.
+
+```css
+/* Without postcss-custom-properties */
+
+body {
+  background: src(var(--foo));
+}
+```
---
+++ test/index.test.ts
@@ -0,0 +1,130 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import plugin = require('../build');
+import postcss, {Result} from 'postcss';
+
+async function run(input: string, options?: plugin.Options): Promise<Result> {
+  return await postcss([plugin(options)]).process(input, {from: undefined});
+}
+
+function assertCompiles(result: Result, output: string): void {
+  expect(result.css).toEqual(output);
+  expect(result.warnings()).toHaveLength(0);
+}
+
+async function assertCompileError(
+  input: string,
+  options?: plugin.Options
+): Promise<void> {
+  await expect(async () => (await run(input, options)).css).rejects.toThrow(
+    'postcss-src:'
+  );
+}
+describe('converts a src() to a URL', () => {
+  it('and removes its quotes', async () => {
+    assertCompiles(
+      await run('a {b: src("https://google.com/")}'),
+      'a {b: url(https://google.com/)}'
+    );
+  });
+
+  describe('preserves its quotes if', () => {
+    it('contains an ident url modifier', async () => {
+      assertCompiles(
+        await run('a {b: src("https://google.com/" foo)}'),
+        'a {b: url("https://google.com/" foo)}'
+      );
+    });
+
+    it('contains a function url modifier', async () => {
+      assertCompiles(
+        await run('a {b: src("https://google.com/" foo(bar))}'),
+        'a {b: url("https://google.com/" foo(bar))}'
+      );
+    });
+
+    it('contains a backslash', async () => {
+      assertCompiles(
+        await run('a {b: src("https://google.com/\\a")}'),
+        'a {b: url("https://google.com/\\a")}'
+      );
+    });
+
+    it('contains a single quote', async () => {
+      assertCompiles(
+        await run('a {b: src("https://google.com/\'")}'),
+        'a {b: url("https://google.com/\'")}'
+      );
+    });
+
+    it('contains a double quote', async () => {
+      assertCompiles(
+        await run("a {b: src('https://google.com/\"')}"),
+        "a {b: url('https://google.com/\"')}"
+      );
+    });
+
+    it('contains a open paren', async () => {
+      assertCompiles(
+        await run('a {b: src("https://google.com/(")}'),
+        'a {b: url("https://google.com/(")}'
+      );
+    });
+
+    it('contains a close paren', async () => {
+      assertCompiles(
+        await run('a {b: src("https://google.com/)")}'),
+        'a {b: url("https://google.com/)")}'
+      );
+    });
+
+    it('contains a space', async () => {
+      assertCompiles(
+        await run('a {b: src("https://google.com/ ")}'),
+        'a {b: url("https://google.com/ ")}'
+      );
+    });
+
+    it('contains a tab', async () => {
+      assertCompiles(
+        await run('a {b: src("https://google.com/\t")}'),
+        'a {b: url("https://google.com/\t")}'
+      );
+    });
+
+    it('contains a non-printable character', async () => {
+      assertCompiles(
+        await run('a {b: src("https://google.com/\x1A")}'),
+        'a {b: url("https://google.com/\x1A")}'
+      );
+    });
+  });
+});
+
+describe('with a variable', () => {
+  it('throws an error with allowNativeSrc: false', async () => {
+    await assertCompileError('a {b: src(var(--foo))}');
+  });
+
+  it('compiles to src with allowNativeSrc: true', async () => {
+    assertCompiles(
+      await run('a {b: src(var(--foo))}', {allowNativeSrc: true}),
+      'a {b: src(var(--foo))}'
+    );
+  });
+});
